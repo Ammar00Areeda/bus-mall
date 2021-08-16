620460de20de01ee6e3672780a7d4e20daec32cc
Ammar00Areeda <akiareeda@yahoo.com>
2021-08-16 17:07:23 +0300

    lab12

--- js/app.js
+++ js/app.js
@@ -6,7 +6,11 @@ let container = document.getElementById('image-container');
 let leftImg = document.getElementById('leftImg');
 let midImg = document.getElementById('midImg');
 let rightImg = document.getElementById('rightImg');
+let botLeftImg = document.getElementById('botLeftImg');
+let botMidImg = document.getElementById('botMidImg');
+let botRightImg = document.getElementById('botRightImg');
 let result = document.getElementById('results');
+
 
 //array for all the products by img
 let productArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
@@ -17,7 +21,7 @@ let attempt = 1;
 
 //const
 function product(productName) {
-  this.productName = productName.split('.',[1]);
+  this.productName = productName.split('.', [1]);
   this.productImg = `imgs/${productName}`;
   this.votes = 0;
   this.views = 0;
@@ -41,22 +45,39 @@ function randomImage() {
 let leftIndex;
 let midIndex;
 let rightIndex;
+let botLeftIndex;
+let botMidIndex;
+let botRightIndex;
+
+let prev = [0,0,0];
+
 function renderImg() {
   leftIndex = randomImage();
   midIndex = randomImage();
   rightIndex = randomImage();
-  while (leftIndex === midIndex === rightIndex) {
+
+  while (leftIndex === rightIndex || leftIndex === midIndex || rightIndex === midIndex || prev.includes(leftIndex) || prev.includes(midIndex) || prev.includes(rightIndex)) {
     leftIndex = randomImage();
     midIndex = randomImage();
     rightIndex = randomImage();
 
   }
+  botRightImg.setAttribute('src', products[prev[2]].productImg);
+  botMidImg.setAttribute('src', products[prev[1]].productImg);
+  botLeftImg.setAttribute('src', products[prev[0]].productImg);
+  prev[0] = leftIndex;
+  prev[1] = midIndex;
+  prev[2] = rightIndex;
+
   leftImg.setAttribute('src', products[leftIndex].productImg);
   midImg.setAttribute('src', products[midIndex].productImg);
   rightImg.setAttribute('src', products[rightIndex].productImg);
+
+
   products[leftIndex].views++;
   products[midIndex].views++;
   products[rightIndex].views++;
+
 }
 renderImg();
 leftImg.addEventListener('click', clickHandler);
@@ -86,6 +107,43 @@ function clickHandler(event) {
     leftImg.removeEventListener('click', clickHandler);
     rightImg.removeEventListener('click', clickHandler);
   }
+}
+function chartRender() {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: gNames,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)'
+        ],
+        borderWidth: 1
+      }, {
+        label: '# of views',
+        data: views,
+        backgroundColor: [
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
 
 
